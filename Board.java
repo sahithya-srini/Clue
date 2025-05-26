b729296c77df1930474d19a71657739484fe0b18
sahithya-srini <sahithya.srini@gmail.com>
2025-05-26 12:18:48 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,18 +1,20 @@
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
+import java.awt.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Board {
     public static final int BOARD_SIZE = 13;
     public static final int CELL_SIZE = 30;
+    public static final int ROOM_GRID_SIZE = 3;
 
     private GridPane grid;
-    private List<Room> rooms = new ArrayList<>();
+    private ArrayList<Room> rooms = new ArrayList<>();
 
     public Board() {
         grid = new GridPane();
@@ -22,28 +24,34 @@ public class Board {
     }
 
     private void setupRooms() {
-        rooms.add(new Room("Lavender Room", Color.LAVENDER, 0, 0, 2, 2));
-        rooms.add(new Room("Blue Room", Color.LIGHTBLUE, 10, 0, 12, 2));
-        rooms.add(new Room("Blush Room", Color.LAVENDERBLUSH, 5, 0, 7, 2));
-        rooms.add(new Room("White Blue Room", Color.ALICEBLUE, 5, 10, 7, 12));
-        rooms.add(new Room("Green Room", Color.LIGHTGREEN, 0, 10, 2, 12));
-        rooms.add(new Room("Coral Room", Color.LIGHTCORAL, 10, 10, 12, 12));
+        rooms.add(new Room("Despicable Me", 0, 0, 2, 2, "file:Resources/DespicableMe.jpeg"));
+        rooms.add(new Room("Tangled", 10, 0, 12, 2, "file:Resources/Tangled.jpeg"));
+        rooms.add(new Room("Mission Impossible", 5, 0, 7, 2, "file:Resources/Mission_Impossible.jpeg"));
+        rooms.add(new Room("Mean Girls", 5, 10, 7, 12, "file:Resources/Mean_Girls.jpeg"));
+        rooms.add(new Room("Avengers", 0, 10, 2, 12, "file:Resources/Avengers.jpeg"));
+        rooms.add(new Room("Cars", 10, 10, 12, 12, "file:Resources/Cars.jpeg"));
     }
 
     private void drawBoard() {
         grid.getChildren().clear();
         for (int row = 0; row < BOARD_SIZE; row++) {
             for (int col = 0; col < BOARD_SIZE; col++) {
-                Rectangle rect = new Rectangle(CELL_SIZE, CELL_SIZE);
                 Room room = getRoomAt(row, col);
-                if (room != null) {
-                    rect.setFill(room.getColor());
-                } else if ((row + col) % 2 == 0) {
-                    rect.setFill(Color.LIGHTGRAY);
-                } else {
+                if (room != null && room.isRoomStart(row, col)) {
+                    // Load the JPEG image from file path or resource
+                    System.out.println("Printing room " + room.getName() + " at " + row + " " + col);
+                    Image image = new Image(room.getImageSrc());
+                    javafx.scene.image.ImageView imgView = new javafx.scene.image.ImageView(image);
+                    imgView.setFitWidth(CELL_SIZE * ROOM_GRID_SIZE);
+                    imgView.setFitHeight(CELL_SIZE * ROOM_GRID_SIZE);
+                    grid.add(imgView, col, row, ROOM_GRID_SIZE, ROOM_GRID_SIZE);
+                } else if (room == null) {
+                    Rectangle rect = new Rectangle(CELL_SIZE, CELL_SIZE);
                     rect.setFill(Color.WHITE);
+                    rect.setStroke(Color.GRAY);      
+                    rect.setStrokeWidth(1);          
+                    grid.add(rect, col, row, 1, 1);
                 }
-                grid.add(rect, col, row);
             }
         }
     }
