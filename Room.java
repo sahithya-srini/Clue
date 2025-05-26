106adb328da093ddc0b218b04c790ea7635465e7
sahithya-srini <sahithya.srini@gmail.com>
2025-05-26 12:17:55 -0700

    Update Room to have image sources.java

--- Room.java
+++ Room.java
@@ -2,20 +2,25 @@ import javafx.scene.paint.Color;
 
 public class Room {
     private String name;
-    private Color color;
     private int rowStart, colStart, rowEnd, colEnd;
+    private String imageSrc;
 
-    public Room(String name, Color color, int rowStart, int colStart, int rowEnd, int colEnd) {
+    public Room(String name, int rowStart, int colStart, int rowEnd, int colEnd, String imageSrc) {
         this.name = name;
-        this.color = color;
         this.rowStart = rowStart;
         this.colStart = colStart;
         this.rowEnd = rowEnd;
         this.colEnd = colEnd;
+        this.imageSrc = imageSrc;
     }
 
     public String getName() { return name; }
-    public Color getColor() { return color; }
+
+    public String getImageSrc() { return imageSrc; }
+
+    public boolean isRoomStart(int row, int col) {
+        return row == rowStart && col == colStart;
+    }
 
     public boolean isInRoom(int row, int col) {
         return row >= rowStart && row <= rowEnd && col >= colStart && col <= colEnd;
